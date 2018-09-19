4281975948a65cdaf71d3a00a694451d92c98148
PriestTmob <mxingzh@163.com>
2018-09-19 14:20:01 +0800

    删除无用注解参数

--- Eureka+Ribbon+Hystrix/spring-cloud-ribbon/src/main/java/com/example/demo/service/HelloService.java
+++ Eureka+Ribbon+Hystrix/spring-cloud-ribbon/src/main/java/com/example/demo/service/HelloService.java
@@ -8,29 +8,28 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class HelloService {
-	
+
 	@Autowired
 	RestTemplate restTemplate;
-	
+
 	@HystrixCommand(
-			groupKey = "",
 			commandKey = "helloService-sayHello",
 			fallbackMethod = "sayHelloDefault")
 	public String sayHelloByRibbon() {
 		return restTemplate.getForObject("http://HELLO-SERVICE/hello", String.class);
 	}
-	
+
 	public String sayHelloDefault() {
 		return "hello service error, this is default say hello method";
 	}
-	
+
 	@HystrixCommand(
 			commandKey = "helloService-sayWhy",
 			fallbackMethod = "sayWhyDefault")
 	public String sayWhyByRibbon() {
 		return restTemplate.getForObject("http://HELLO-SERVICE/why", String.class);
 	}
-	
+
 	public String sayWhyDefault() {
 		return "hello service error, this is default say why method";
 	}
