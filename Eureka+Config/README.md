b4121d0edcdd89c26d8a84fdcad1e33461675e28
PriestTmob <mxingzh@163.com>
2018-09-20 17:49:01 +0800

    Demo：Eureka+Config

---
+++ Eureka+Config/README.md
@@ -0,0 +1,25 @@
+## 说明
+
+当前 Demo 由 3 个项目构成：
+
+* Eureka Server
+
+* Eureka Client (Config Server)
+
+* Eureka Client (Config Client)
+
+目前实现功能：
+
+* Eureka 服务注册与发现
+
+* Config Server 将配置放在本地
+
+* Config Client 中使用的配置交由 Config Server 管理，并在代码中获取配置在 Server 端的值
+
+## 更新记录
+
+#### 0. 2018.09.20
+
+* 服务注册与发现
+
+* 使用 Config 管理配置
---
+++ Eureka+Config/spring-cloud-config-server/src/main/java/com/example/demo/SpringCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
