65900e63a509ed2a57aa65ed46b6bb86322b9a84
PriestTomb <mxingzh@163.com>
2018-09-25 22:20:08 +0800

    删除多余import

--- Eureka+Config/spring-cloud-config-server/src/main/java/com/example/demo/SpringCloudConfigServerApplication.java
+++ Eureka+Config/spring-cloud-config-server/src/main/java/com/example/demo/SpringCloudConfigServerApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
